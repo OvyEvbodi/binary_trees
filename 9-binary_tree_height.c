3ef27928436c3638a48f88635ee870264700d9a2
Ovy-Evbodi <evbodiovo@gmail.com>
2023-02-28 15:24:37 +0100

    Fix(binary_tree_height): Amend return statement to check both children

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,5 +12,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	return (1 + binary_tree_height(tree->left));
+	return (1 + (binary_tree_height(tree->left) ||
+		binary_tree_height(tree->right)));
 }

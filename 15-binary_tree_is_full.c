e3f1f104b909947da114139bc2cbf9c3343d9363
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-03 00:41:16 +0100

    Chore: Fix documentation

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,6 +13,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	return (full_tree(tree));
 }
 
+/**
+ * full_tree - checks if a binary tree is full
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1, if tree is full,
+ * otherwise, 0, if not full, or tree is NULL
+ */
 
 int full_tree(const binary_tree_t *tree)
 {
@@ -24,4 +31,3 @@ int full_tree(const binary_tree_t *tree)
 		return (full_tree(tree->left) && full_tree(tree->right));
 	return (0);
 }
-

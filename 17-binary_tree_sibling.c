95825639ed71e759ff6f2c0fc9a63bb103fedb7e
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-03 10:52:43 +0100

    Fix(*): Change return value for when tree is NULL

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_sibling - finds the sibling of a node
- * @tree: a pointer to the node to find the sibling
+ * @node: a pointer to the node to find the sibling
  *
  * Return: a pointer to the sibling node,
  * or NULL, if node is NULL, the parent is NULL, or node has no sibling
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node && node->parent)
 	{
-		if (node->parent->left != node)
+		if (node->parent->left && node->parent->left != node)
 			return (node->parent->left);
-		else
+		else if (node->parent->right)
 			return (node->parent->right);
 	}
 	FAIL;
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_uncle - finds the uncle of a node
- * @tree: a pointer to the node to find the uncle
+ * @node: a pointer to the node to find the uncle
  *
  * Return: a pointer to the uncle node,
  * or NULL, if node is NULL, the parent is NULL, or node has no uncle
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node && node->parent->parent)
 	{
-		if (node->parent->parent->left != node->parent)
+		if (node->parent->parent->left && node->parent->parent->left != node->parent)
 			return (node->parent->parent->left);
-		else
+		else if (node->parent->parent->right)
 			return (node->parent->parent->right);
 	}
 	FAIL;

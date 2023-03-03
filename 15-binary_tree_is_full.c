53b6da762e118068c3c8e2dd06a72f4db124e95b
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-03 10:16:11 +0100

    Fix(binary_tree_is_full): Change return value for when tree is NULL

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -24,7 +24,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 int full_tree(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (1);
+		return (0);
 	if ((!tree->left && !tree->right))
 		return (1);
 	if (tree->left && tree->right)

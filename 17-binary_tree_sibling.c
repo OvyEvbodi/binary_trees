7f02cb3606a637c902e5f8a7d34486ba52367cba
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-04 01:28:38 +0100

    Fix(*): Update return

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		if (node->parent->left && node->parent->left != node)
 			return (node->parent->left);
-		if (node->parent->right)
+		if (node->parent->right && node->parent->right != node)
 			return (node->parent->right);
 	}
 	FAIL;
